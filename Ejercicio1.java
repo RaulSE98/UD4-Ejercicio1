0a9d6d8605d3de1aa75f403389a8ad16e63a3004
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-07-07 12:04:36 +0200

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,24 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1 = 10;
+		int n2 = 5;
+		int resultado;
+		
+		//Suma
+		System.out.println("resultado n1 + n2 = " + (resultado = n1 + n2));
+		//Resta
+		System.out.println("resultado n1 - n2 = " + (resultado = n1 - n2));
+		//Multiplicacion
+		System.out.println("resultado n1 * n2 = " + (resultado = n1 * n2));
+		//Division
+		System.out.println("resultado n1 / n2 = " + (resultado = n1 / n2));
+		//Modulo
+		System.out.println("resultado n1 % n2 = " + (resultado = n1 % n2));
+
+	}
+
+}
